9a241a1376847ab8a404c51eefed12ae3c839c95
Pham Thanh Hung <thanhhungsky98@gmail.com>
2024-04-17 22:23:24 +0700

    #add api

--- ezgenerator/server.js
+++ ezgenerator/server.js
@@ -1,17 +1,30 @@
 // server.js
 const express = require('express');
 const next = require('next');
+const axios = require('axios');
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
+const randomUserApi = "https://randomuser.me/api/";
+const randomOthers = "https://randommer.io/";
 
 app.prepare().then(() => {
   const server = express();
 
-  // Custom route example
-  server.get('/api/data', (req, res) => {
-    res.json({ message: 'Hello from Express.js API' });
+  server.get('/api/random-user', async (req, res) => {
+    try {
+      const { gender, nat } = req.query;
+      const apiUrl = `${randomUserApi}?gender=${gender || 'all'}&nat=${nat || 'us'}`;
+      
+      const response = await axios.get(apiUrl);
+      const data = response.data;
+      res.json(data);
+    } catch (error) {
+      console.error('Error fetching random user:', error);
+      res.status(500).send('Error fetching random user');
+    }
+  
   });
 
   server.all('*', (req, res) => {
