3f1b9482dd254d200388bc0e0ac0df478a30e477
ericnguyen88 <eric.nguyen@ubteam.com>
2024-04-17 23:36:31 +0700

    First design

--- ezgenerator/src/app/page.tsx
+++ ezgenerator/src/app/page.tsx
@@ -1,113 +1,210 @@
 import Image from "next/image";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDownload } from "@fortawesome/free-solid-svg-icons";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <><header className="bg-dark text-white py-5">
+      <div className="container">
+        <div className="row pb-5">
+          <div className="header-inner-div col-md-12 text-center">
+            <h2 className="fs-1 fw-normal text-uppercase">Random User Generator</h2>
+            <p className="fw-lighter">Random user generator is a practice project made by random user api and
+              bootstrap 5. By this website you can generate a random person info and use anywhere.</p>
+              <div className="mb-3 container">
+                <div className="row">
+                  <div className="col">
+                  <label className="form-label title-select-top">Gender</label>
+                  <select className="form-select" aria-label="Random">
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                  </select>
+                  </div>
+                  <div className="col">
+                  <label className="form-label title-select-top">Age</label>
+                  <select className="form-select" aria-label="Random">
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                  </select>
+                  </div>
+                  <div className="col">
+                  <label className="form-label title-select-top">Country</label>
+                  <select className="form-select" aria-label="Random">
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                  </select>
+                  </div>
+                  <div className="col">
+                  <label className="form-label title-select-top">State</label>
+                  <select className="form-select" aria-label="Random">
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                  </select>
+                  </div>
+                  <div className="col">
+                    <button type="button" className="btn btn-success py-3 px-5 rounded-2 fw-semibold">Generate <FontAwesomeIcon icon={faDownload} /></button>
+                  </div>
+                </div>
+              </div>
+          </div>
         </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </header><main>
+        <div className="container mb-5">
+          <div className="row">
+            <div className="col-md-3" id="randomUserDisplaySection">
+                <div className="card text-center shadow-lg">
+                  <ul className="nav nav-pills flex-column mb-auto sidenav-left">
+                    <li className="nav-item">
+                      <a href="#" className="nav-link active" aria-current="page">
+                      Identity Generator
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#" className="nav-link link-dark">
+                      Random Text
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#" className="nav-link link-dark">
+                      Fake Email
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#" className="nav-link link-dark">
+                      Fake Credit Card Generator
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#" className="nav-link link-dark">
+                      Fake Credit Card Validator
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#" className="nav-link link-dark">
+                      Fake Driver's License
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#" className="nav-link link-dark">
+                      Fake Company
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#" className="nav-link link-dark">
+                      Fake Phone Number
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#" className="nav-link link-dark">
+                      Fake Social Security Number
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+            </div>
+            <div className="col-md-7" id="randomUserDisplaySection">
+              <div className="card text-center shadow-lg">
+                <div className="card-header pt-5 shadow-sm"><img src="https://randomuser.me/api/portraits/women/89.jpg" className="user-rounded-image img-fluid" id="displayUserPhoto"></img></div>
+                <div className="card-body mb-4 mt-5">
+                  <h3 className="card-title pt-2 fs-2" id="displayUserName">Seline Giraud</h3>
+                  <table className="table table-bordered mt-4">
+                    <tbody className="text-start" id="displayUserInfo">
+                      <tr className="">
+                        <td className="col-1 text-center"><i className="fa-solid fa-user fs-4 text-muted"></i>
+                        </td>
+                        <th scope="col-2">Full Name: </th>
+                        <td className="col-6" id="displayUserFullName"></td>
+                        <td className="col-2 text-center"><button type="button"
+                          className="btn btn-outline-secondary" id="copyUserFullName"><i
+                            className="fa-regular fa-clipboard me-2"></i>Copy</button></td>
+                      </tr>
+                      <tr>
+                        <td className="col-1 text-center"><i
+                          className="fa-solid fa-envelope fs-4 text-muted"></i></td>
+                        <th scope="col-2">Email Address: </th>
+                        <td className="col-6" id="displayUserEmail"></td>
+                        <td className="col-2 text-center"><button type="button"
+                          className="btn btn-outline-secondary" id="copyUserEmail"><i
+                            className="fa-regular fa-clipboard me-2"></i>Copy</button></td>
+                      </tr>
+                      <tr>
+                        <td className="col-1 text-center"><i className="fa-solid fa-phone fs-4 text-muted"></i>
+                        </td>
+                        <th scope="col-2">Phone Number: </th>
+                        <td className="col-6" id="displayUserPhoneNumber"></td>
+                        <td className="col-2 text-center"><button type="button"
+                          className="btn btn-outline-secondary" id="copyUserPhoneNumber"><i
+                            className="fa-regular fa-clipboard me-2"></i>Copy</button></td>
+                      </tr>
+                      <tr>
+                        <td className="col-1 text-center"><i className="fa-solid fa-user fs-4 text-muted"></i>
+                        </td>
+                        <th scope="col-2">Gander: </th>
+                        <td className="col-6 text-capitalize" id="displayUserGander"></td>
+                        <td className="col-2 text-center"><button type="button"
+                          className="btn btn-outline-secondary" id="copyUserGander"><i
+                            className="fa-regular fa-clipboard me-2"></i>Copy</button></td>
+                      </tr>
+                      <tr>
+                        <td className="col-1 text-center"><i
+                          className="fa-regular fa-calendar fs-4 text-muted"></i></td>
+                        <th scope="col-2">Date of Birth: </th>
+                        <td className="col-6" id="displayUserDOB"></td>
+                        <td className="col-2 text-center"><button type="button"
+                          className="btn btn-outline-secondary" id="copyUserDOB"><i
+                            className="fa-regular fa-clipboard me-2"></i>Copy</button></td>
+                      </tr>
+                      <tr>
+                        <td className="col-1 text-center"><i
+                          className="fa-solid fa-location-dot fs-4 text-muted"></i></td>
+                        <th scope="col-2">Address: </th>
+                        <td className="col-6" id="displayUserAddress"></td>
+                        <td className="col-2 text-center"><button type="button"
+                          className="btn btn-outline-secondary" id="copyUserAddress"><i
+                            className="fa-regular fa-clipboard me-2"></i>Copy</button></td>
+                      </tr>
+                      <tr>
+                        <td className="col-1 text-center"><i
+                          className="fa-solid fa-map-location-dot fs-4 text-muted"></i></td>
+                        <th scope="col-2">Post Code: </th>
+                        <td className="col-6" id="displayUserPostcode"></td>
+                        <td className="col-2 text-center"><button type="button"
+                          className="btn btn-outline-secondary" id="copyUserPostcode"><i
+                            className="fa-regular fa-clipboard me-2"></i>Copy</button></td>
+                      </tr>
+                      <tr>
+                        <td className="col-1 text-center"><i
+                          className="fa-solid fa-id-card fs-4 text-muted"></i></td>
+                        <th scope="col-2">SSN Number: </th>
+                        <td className="col-6" id="displayUserSSNNumber"></td>
+                        <td className="col-2 text-center"><button type="button"
+                          className="btn btn-outline-secondary" id="copyUserSSNNumber"><i
+                            className="fa-regular fa-clipboard me-2"></i>Copy</button></td>
+                      </tr>
+                    </tbody>
+                  </table>
+                  <button type="button" className="btn btn-primary py-3 px-5 mt-3 rounded-2 fw-semibold">Generate New User</button>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-2"></div>
+          </div>
+        </div>
+      </main><footer className="bg-dark py-5">
+        <p className="text-white text-center mb-0">Copyright @ 2022. All Right Reserved Developed by Azhar Anowar</p>
+      </footer><div className="toast-container position-fixed bottom-0 end-0 p-3">
+        <div id="copiedToClipboardToast" className="toast" role="alert" aria-live="assertive" aria-atomic="true">
+          <div className="toast-header">
+            <strong className="me-auto text-success"><i className="fa-regular fa-clipboard me-2"></i>Copied to clipboard!</strong>
+            <button type="button" className="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+          </div>
+          <div className="toast-body text-secondary fw-normal" id="copiedToClipboardMessage">
+          </div>
+        </div>
+      </div></>
   );
 }
