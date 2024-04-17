21778e56796119cf765f797e9ca14786c843bea4
Hung Pham Thanh <hung.phamthanh@nashtechglobal.com>
2024-04-17 14:22:12 +0700

    #init project

---
+++ ezgenerator/server.js
@@ -0,0 +1,25 @@
+// server.js
+const express = require('express');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  // Custom route example
+  server.get('/api/data', (req, res) => {
+    res.json({ message: 'Hello from Express.js API' });
+  });
+
+  server.all('*', (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(3000, (err) => {
+    if (err) throw err;
+    console.log('> Ready on http://localhost:3000');
+  });
+});
