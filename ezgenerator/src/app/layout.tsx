18652e77e13e1ef6ff002f6453668259d3a16726
ericnguyen88 <eric.nguyen@ubteam.com>
2024-05-27 02:31:58 +0700

    chore: Add Telegram link to the bottom right corner of the page

--- ezgenerator/src/app/layout.tsx
+++ ezgenerator/src/app/layout.tsx
@@ -24,7 +24,14 @@ export default function RootLayout({
       <head>
         <meta name="google-site-verification" content="-lPnpQY3npvXBA2AovIMUwhCr8A87FsQolziiMJwDmo" />
       </head>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <a href="https://t.me/sptoolsfree"
+        style={{ position: "fixed", bottom: 0, right: 0, margin: 20, zIndex: 999, borderRadius: "50%" }} target="_blank"
+        rel="nofollow" className="tm-btn btn-dark">
+        <img width="60px" height="60px" alt="Temp Mail Extension for Telegram" className="lazy" src="./telegram.svg"/>
+      </a>
+        {children}
+        </body>
     </html>
   );
 }
